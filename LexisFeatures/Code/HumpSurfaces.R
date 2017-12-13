0e8f091bb4373a5111a8a38f92b924a5d8cf4e0b
Tim Riffe <tim.riffe@gmail.com>
2017-12-13 11:28:30 +0100

    getting some files in the repo. FIrst one is just a stub

---
+++ LexisFeatures/Code/HumpSurfaces.R
@@ -0,0 +1,18 @@
+
+I.am.Tim <- FALSE
+if (I.am.Tim){
+setwd("/home/tim/Dropbox/compH/Reproduction")
+MxcResults <- local(get(load("Data/Mxclong_results.RData")))
+head(MxcResults)
+
+# All cause mortality.
+library(reshape2)
+
+
+
+}
+
+
+
+
+
