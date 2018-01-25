a295f81fc55f51760de1119b09a36e7b879429f4
Tim Riffe <tim.riffe@gmail.com>
2018-01-25 13:27:52 +0100

    added tidy data to use case 1, corrected smooth version

--- scripts/Tim/HumpSurfaces.R
+++ scripts/Tim/HumpSurfaces.R
@@ -1,9 +1,8 @@
 
 I.am.Tim <- FALSE
 if (I.am.Tim){
-	setwd("/home/tim/git/LexisFeatures/LexisFeatures")
+	setwd("/home/tim/git/LexisFeatures")
 	MxcResults <- local(get(load("/home/tim/Dropbox/compH/Reproduction/Data/Mxclong_results.RData")))
-	head(MxcResults)
 	
 # All cause mortality.
 	library(reshape2)
@@ -11,8 +10,28 @@ if (I.am.Tim){
 	HumpMales   <- acast(MxcResults[MxcResults$Sex == "m" & MxcResults$Cause == 0, ], Age~Year, value.var = "Hump")
 	HumpMalesSm <- acast(MxcResults[MxcResults$Sex == "m" & MxcResults$Cause == 0, ], Age~Year, value.var = "HumpSm")
 	
-	write.csv(HumpMales, file = "Data/HumpMales.csv")
-	write.csv(HumpMales, file = "Data/HumpMalesSm.csv")
+	write.csv(HumpMales, file = "data/csv_matrix/use_case_1/HumpMales.csv")
+	write.csv(HumpMalesSm, file = "data/csv_matrix/use_case_1/HumpMalesSm.csv")
+	
+# tidy variables
+#	use case
+#	use case modifier (e.g. sample size based on)
+#	age
+#	year
+#	value (z, fill
+	
+	HumpMalesTidy         <- melt(HumpMales, varnames = c("age","year"))
+	HumpMalesTidy$sex     <- "males"
+	HumpMalesTidy$usecase <- 1
+	HumpMalesTidy$modifier <- "all-cause. smooth over age but not year"
+	
+	HumpMalesSmTidy         <- melt(HumpMalesSm, varnames = c("age","year"))
+	HumpMalesSmTidy$sex     <- "males"
+	HumpMalesSmTidy$usecase <- 1
+	HumpMalesSmTidy$modifier <- "all-cause. smoothed with MortSmooth"
+	
+	write.csv(HumpMales, file = "data/csv_tidy/use_case_1/HumpMales.csv")
+	write.csv(HumpMalesSm, file = "data/csv_tidy/use_case_1/HumpMalesSm.csv")
 }
 
 
